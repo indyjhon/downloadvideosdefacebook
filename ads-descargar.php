789b5115659074cdd442e16408d6cf1e50e46466
indyjhon <70450087+indyjhon@users.noreply.github.com>
2020-08-29 17:21:11 -0500

    Update ads-descargar.php

--- ads-descargar.php
+++ ads-descargar.php
@@ -1,12 +1,2 @@
 
-<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-<!-- bloque cuadrado -->
-<ins class="adsbygoogle"
-     style="display:block"
-     data-ad-client="ca-pub-1547291314439953"
-     data-ad-slot="2235220213"
-     data-ad-format="auto"
-     data-full-width-responsive="true"></ins>
-<script>
-     (adsbygoogle = window.adsbygoogle || []).push({});
-</script>
+tu codigo ads
